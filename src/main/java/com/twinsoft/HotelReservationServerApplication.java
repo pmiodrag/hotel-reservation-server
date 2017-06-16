2eb6027e973b4ea01e3e183de28168e4395fa10a
ms.pavkovic <ms.pavkovic@levi9.com>
2017-06-16 13:52:47 +0200

    clean up

--- src/main/java/com/twinsoft/HotelReservationServerApplication.java
+++ src/main/java/com/twinsoft/HotelReservationServerApplication.java
@@ -1,16 +1,12 @@
 package com.twinsoft;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Configuration;
 
 
 @SpringBootApplication
-@Configuration
-@EnableAutoConfiguration
 public class HotelReservationServerApplication extends SpringBootServletInitializer {
 	
 	@Override
