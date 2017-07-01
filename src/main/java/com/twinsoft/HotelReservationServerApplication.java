0cda6496e3feba1185f3b1a48e6b53c1a93c0375
miodrag <mpavkovic@gmail.com>
2017-07-01 19:38:58 +0200

    use tokenstore

--- src/main/java/com/twinsoft/HotelReservationServerApplication.java
+++ src/main/java/com/twinsoft/HotelReservationServerApplication.java
@@ -4,9 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
+
+import com.twinsoft.security.OAuth2AuthorizationServerConfig;
+
+import nl.powerhouse.platform.auth.conf.AppOAuth2ServerConfiguration;
+import nl.powerhouse.platform.auth.conf.AppWebSecurityConfiguration;
 
 
 @SpringBootApplication
+@Import({ OAuth2AuthorizationServerConfig.class })
 public class HotelReservationServerApplication extends SpringBootServletInitializer {
 	
 	@Override
