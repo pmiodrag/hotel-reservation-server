a511c6146b3f3439c1a5c8b0668b3f730be8d320
miodrag <mpavkovic@gmail.com>
2017-07-01 19:46:03 +0200

    Revert "use tokenstore"
    This reverts commit 0cda6496e3feba1185f3b1a48e6b53c1a93c0375.

--- src/main/java/com/twinsoft/security/AppWebSecurityConfiguration.java
+++ src/main/java/com/twinsoft/security/AppWebSecurityConfiguration.java
@@ -1,47 +1,48 @@
-///**
-// *
-// */
-//package com.twinsoft.security;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-///**
-// * @author Miodrag Pavkovic
-// */
-//@Configuration
-//@EnableWebSecurity
-//public class AppWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-//
-//    @Autowired
-//    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER").and().withUser("admin")
-//                .password("password").roles("USER", "ADMIN");
-//    }
-//    
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//     http
-//        .authorizeRequests()
-//            .antMatchers(HttpMethod.OPTIONS).permitAll()
-//            .anyRequest().authenticated()
-//            .and().httpBasic()
-//            .and().csrf().disable();
-//    }
-//    
-//    @Override
-//    @Bean
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
-//        
-//}
-//    
+/**
+ *
+ */
+package com.twinsoft.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Zolt Egete z.egete@levi9.com
+ * @since Aug 17, 2015
+ */
+@Configuration
+@EnableWebSecurity
+public class AppWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER").and().withUser("admin")
+                .password("password").roles("USER", "ADMIN");
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+     http
+        .authorizeRequests()
+            .antMatchers(HttpMethod.OPTIONS).permitAll()
+            .anyRequest().authenticated()
+            .and().httpBasic()
+            .and().csrf().disable();
+    }
+    
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+        
+}
+    
--- src/main/java/com/twinsoft/security/CustomTokenEnhancer.java
+++ src/main/java/com/twinsoft/security/CustomTokenEnhancer.java
@@ -1,41 +1,32 @@
-///**
-// * 
-// */
-//package com.twinsoft.security;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
-//import org.springframework.security.oauth2.common.OAuth2AccessToken;
-//import org.springframework.security.oauth2.provider.OAuth2Authentication;
-//import org.springframework.security.oauth2.provider.token.TokenEnhancer;
-//
-///**
-// * @author Miodrag Pavkovic
-// */
-//public class CustomTokenEnhancer implements TokenEnhancer {
-//	
-//	private static final String TENANT_ID = "tenant_id";
-//    private static final int TENANT_SYSTEM_ID = 1;
-//	/* (non-Javadoc)
-//	 * @see org.springframework.security.oauth2.provider.token.TokenEnhancer#enhance(org.springframework.security.oauth2.common.OAuth2AccessToken, org.springframework.security.oauth2.provider.OAuth2Authentication)
-//	 */
-//	  @Override
-//	    public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
-//	        final Map<String, Object> additionalInfo = new HashMap<>(accessToken.getAdditionalInformation());
-//	        // Adds the hard coded tenant ID 1 to the JWT access token
-//	        additionalInfo.put(TENANT_ID, authentication.getName() + TENANT_SYSTEM_ID);
-//	        ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
-//	        return accessToken;
-//	    }
-////	  @Override public OAuth2AccessToken enhance(final OAuth2AccessToken accessToken, final OAuth2Authentication authentication) {
-////	        final Map<String, Object> additionalInformation = new HashMap<>(accessToken.getAdditionalInformation());
-////	        additionalInformation.put(TENANT_ID, TENANT_SYSTEM_ID);
-////	        final DefaultOAuth2AccessToken customAccessToken = new DefaultOAuth2AccessToken(accessToken);
-////	        customAccessToken.setAdditionalInformation(additionalInformation);
-////
-////	        return super.enhance(customAccessToken, authentication);
-////	    }
-//
-//}
+/**
+ * 
+ */
+package com.twinsoft.security;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+
+/**
+ * @author Miodrag Pavkovic
+ */
+public class CustomTokenEnhancer implements TokenEnhancer {
+
+	/* (non-Javadoc)
+	 * @see org.springframework.security.oauth2.provider.token.TokenEnhancer#enhance(org.springframework.security.oauth2.common.OAuth2AccessToken, org.springframework.security.oauth2.provider.OAuth2Authentication)
+	 */
+	  @Override
+	    public OAuth2AccessToken enhance(
+	     OAuth2AccessToken accessToken, 
+	     OAuth2Authentication authentication) {
+	        Map<String, Object> additionalInfo = new HashMap<>();
+	        additionalInfo.put("organization", authentication.getName());
+	        ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
+	        return accessToken;
+	    }
+
+}
