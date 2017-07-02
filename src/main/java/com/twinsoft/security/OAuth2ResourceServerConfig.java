4268dd9e0c362e42c712819124cd3435287260c4
miodrag <mpavkovic@gmail.com>
2017-07-02 17:30:16 +0200

    authorization

--- src/main/java/com/twinsoft/security/OAuth2ResourceServerConfig.java
+++ src/main/java/com/twinsoft/security/OAuth2ResourceServerConfig.java
@@ -1,63 +1,63 @@
-/**
- * 
- */
-package com.twinsoft.security;
-
-import java.io.IOException;
-
-import org.apache.commons.io.IOUtils;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-
-/**
- * @author Miodrag Pavkovic
- */
-public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
-    @Override
-    public void configure(ResourceServerSecurityConfigurer config) {
-        config.tokenServices(tokenServices());
-    }
- 
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(accessTokenConverter());
-    }
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        Resource resource = new ClassPathResource("public.txt");
-        String publicKey = null;
-        try {
-            publicKey = IOUtils.toString(resource.getInputStream());
-        } catch (final IOException e) {
-            throw new RuntimeException(e);
-        }
-        converter.setVerifierKey(publicKey);
-        return converter;
-    }
- 
+///**
+// * 
+// */
+//package com.twinsoft.security;
+//
+//import java.io.IOException;
+//
+//import org.apache.commons.io.IOUtils;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.core.io.ClassPathResource;
+//import org.springframework.core.io.Resource;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+//import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+//import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+//import org.springframework.security.oauth2.provider.token.TokenStore;
+//import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+//import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+//
+///**
+// * @author Miodrag Pavkovic
+// */
+//public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
+//    @Override
+//    public void configure(ResourceServerSecurityConfigurer config) {
+//        config.tokenServices(tokenServices());
+//    }
+// 
+//    @Bean
+//    public TokenStore tokenStore() {
+//        return new JwtTokenStore(accessTokenConverter());
+//    }
 //    @Bean
 //    public JwtAccessTokenConverter accessTokenConverter() {
 //        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-//        converter.setSigningKey("123");
+//        Resource resource = new ClassPathResource("public.txt");
+//        String publicKey = null;
+//        try {
+//            publicKey = IOUtils.toString(resource.getInputStream());
+//        } catch (final IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        converter.setVerifierKey(publicKey);
 //        return converter;
 //    }
- 
-    @Bean
-    @Primary
-    public DefaultTokenServices tokenServices() {
-        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore());
-        return defaultTokenServices;
-    }
-
-
-}
+// 
+////    @Bean
+////    public JwtAccessTokenConverter accessTokenConverter() {
+////        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+////        converter.setSigningKey("123");
+////        return converter;
+////    }
+// 
+//    @Bean
+//    @Primary
+//    public DefaultTokenServices tokenServices() {
+//        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+//        defaultTokenServices.setTokenStore(tokenStore());
+//        return defaultTokenServices;
+//    }
+//
+//
+//}
